3259f9f9c5e83c3f94a7d89ce77be8a10e9b1562
James Thompson <me@jtgraphic.net>
2017-01-07 01:34:28 -0500

    restoring everything

--- restore.bat
+++ restore.bat
@@ -1,1 +1,3 @@
 copy scenes\jtgraphic.json %AppData%\obs-studio\basic\scenes\jtgraphic.json
+copy profiles\jtgraphic\basic.ini "%AppData%\obs-studio\basic\profiles\jtgraphic\basic.ini"
+copy global.ini "%AppData%\obs-studio\global.ini"
