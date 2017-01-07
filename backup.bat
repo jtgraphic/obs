dfb1a20ca9b29bfc0bf034098cdfac84646d0e30
James Thompson <me@jtgraphic.net>
2017-01-07 01:33:24 -0500

    adding to backup/restore; backing up

--- backup.bat
+++ backup.bat
@@ -1,1 +1,3 @@
 copy "%AppData%\obs-studio\basic\scenes\jtgraphic.json" scenes\jtgraphic.json
+copy "%AppData%\obs-studio\basic\profiles\jtgraphic\basic.ini" profiles\jtgraphic\basic.ini
+copy "%AppData%\obs-studio\global.ini" global.ini
